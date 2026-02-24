c406e5126f29da1daff939d562acdf5d7e1850a3
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-24 13:18:40 +0530

    feat: update the square logic to accept value from prop

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 export default function Square(props) {
+    const [value, setValue] = React.useState(props.value);
+    function handleClick() {
+        setValue(value === 'X' ? 'O' : 'X');
+    }
   return (
-      <button className='flex justify-center items-center w-16 h-16 border border-gray-300 cursor-pointer' onClick={props.onClick}>
-        <p className="text-xl font-bold">{props.value}</p>
-      </button>
+    <button 
+        className='flex justify-center items-center w-28 h-28 border border-gray-300 cursor-pointer' 
+        onClick={props.onClick}
+    >
+        {props.value}
+    </button>
   )
 }
