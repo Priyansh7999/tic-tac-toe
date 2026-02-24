abf3382ef98ee0eac09c1ba7fa90e5f5d197a1d2
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-24 15:43:47 +0530

    fix: change  Board component to render tic-tac-toe grid

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,86 +1,16 @@
-import React, { useEffect, useState } from 'react'
-import Square from './Square'
-export default function Board() {
+import Square from "./Square";
 
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [isNext, setIsNext] = useState(true);
-    let winner = calculateWinner(squares);
-    const isDraw = !winner && squares.every(box => box !== null);
+export default function Board({ squares, onSquareClick }) {
 
-    const handleSquareClick = (index) => {
-        if (squares[index] || winner || isDraw) return;
-        const newSquares = [...squares];
-        newSquares[index] = isNext ? 'X' : 'O';
-        setSquares(newSquares);
-        setIsNext(!isNext);
-    }
-
-    function calculateWinner(squares) {
-        const patterns = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8],
-            [0, 4, 8],
-            [2, 4, 6]
-        ];
-        for (let i = 0; i < patterns.length; i++) {
-            const [a, b, c] = patterns[i];
-            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }
-        }
-        return null;
-    }
-
-    const resetGame = () => {
-        setSquares(Array(9).fill(null));
-        setIsNext(true);
-    }
     return (
-        <div className="flex flex-col items-center justify-center w-full">
-            <div className='w-2/3 mt-10 px-4 py-2 bg-custom-dark-blue rounded mb-6 text-center flex justify-between'>
-                <p className='text-custom-blue font-mono'>Player 1 : X</p>
-                <p className='text-custom-green font-mono'>Player 2 : O</p>
-            </div>
-            <div className=''>
-                {
-                    winner ? (
-                        <h2 className='text-2xl font-mono text-green-500 mb-4'>
-                            Winner: {winner==='X' ? 'Player 1' : 'Player 2'}
-                        </h2>
-                    ) : isDraw ? (
-                        <h2 className='text-2xl font-mono text-yellow-400 mb-4'>
-                            Draw
-                        </h2>
-                    ) : (
-                        <h2 className='text-2xl font-mono text-blue-400 mb-4'>
-                            Next Player: {isNext ? 'X' : 'O'}
-                        </h2>
-                    )
-                }
-            </div>
-
-            <div className="grid grid-cols-3 gap-2">
-                {squares.map((sq, i) => (
-                    <Square
-                        key={i}
-                        value={sq}
-                        onClick={() => handleSquareClick(i)}
-                    />
-                ))}
-            </div>
-            <div className='flex justify-center'>
-                <button
-                    className="mt-6 px-4 py-2 font-mono bg-blue-500 text-white rounded hover:bg-blue-600"
-                    onClick={resetGame}
-                >
-                    Reset Game
-                </button>
-            </div>
-
+        <div className="grid grid-cols-3 gap-2">
+            {squares.map((sq, i) => (
+                <Square
+                    key={i}
+                    value={sq}
+                    onClick={() => onSquareClick(i)}
+                />
+            ))}
         </div>
-    )
+    );
 }
