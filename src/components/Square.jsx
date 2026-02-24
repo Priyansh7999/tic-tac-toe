76b5c3be22e6059c7b50171ed18a93035cec3d25
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-24 15:50:16 +0530

    fix: remove the handle-click code from Square component

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 export default function Square(props) {
-    const [value, setValue] = React.useState(props.value);
-    function handleClick() {
-        setValue(value === 'X' ? 'O' : 'X');
-    }
   return (
     <button 
         className='flex justify-center text-2xl bg-custom-box text-custom-red font-mono items-center w-28 h-28 cursor-pointer' 
