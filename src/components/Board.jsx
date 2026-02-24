462928e774615ee3796be7f3bc07a6e36f567644
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-24 13:16:40 +0530

    refactor: use map to display each square on board

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './Square'
-
 export default function Board() {
-  return (
-    <div className="grid grid-cols-3 gap-2 w-48 h-48">
-      <Square value="1" onClick={() => console.log('Square 1 clicked')} />
-      <Square value="2" onClick={() => console.log('Square 2 clicked')} />
-      <Square value="3" onClick={() => console.log('Square 3 clicked')} />
-      <Square value="4" onClick={() => console.log('Square 4 clicked')} />
-      <Square value="5" onClick={() => console.log('Square 5 clicked')} />
-      <Square value="6" onClick={() => console.log('Square 6 clicked')} />
-      <Square value="7" onClick={() => console.log('Square 7 clicked')} />
-      <Square value="8" onClick={() => console.log('Square 8 clicked')} />
-      <Square value="9" onClick={() => console.log('Square 9 clicked')} />
-    </div>
-  )
+
+    const [squares, setSquares] = useState(Array(9).fill(null));
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div className='relative flex justify-center items-center mb-6'>
+                <p>Player 1 : X</p>
+                <p>Player 2 : O</p>
+            </div>
+
+
+            <div className="grid grid-cols-3 gap-2">
+                {squares.map((sq, i) => (
+                    <Square
+                        key={i}
+                        value={sq}
+                    />
+                ))}
+            </div>
+
+        </div>
+    )
 }
